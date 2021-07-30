eed4f3f7d0c504c4fc123416369d88a56fb5aee3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-07-30 19:47:44 -0300

    Relatório de quantidade de produtos por cor

--- src/main/java/br/com/rchlo/main/ProductColorsReportMain.java
+++ src/main/java/br/com/rchlo/main/ProductColorsReportMain.java
@@ -1,2 +1,23 @@
-package br.com.rchlo.main;public class ProductColorsReportMain {
+package br.com.rchlo.main;
+
+import br.com.rchlo.data.ProductRepository;
+import br.com.rchlo.domain.Product;
+import br.com.rchlo.dto.ProductsReportResult;
+import br.com.rchlo.dto.ProductsReportResult.ProductsReportResultItem;
+import br.com.rchlo.service.ProductColorsReport;
+
+import java.util.List;
+
+public class ProductColorsReportMain {
+
+    public static void main(String[] args) {
+        var productColorsReport = new ProductColorsReport();
+        List<Product> allProducts = ProductRepository.all();
+
+        ProductsReportResult reportResult = productColorsReport.report(allProducts);
+        for (ProductsReportResultItem reportItem : reportResult) {
+            System.out.printf("%s: %s%n", reportItem.getColor(), reportItem.getQuantity());
+        }
+    }
+
 }
--- src/main/java/br/com/rchlo/service/ProductColorsReport.java
+++ src/main/java/br/com/rchlo/service/ProductColorsReport.java
@@ -1,2 +1,16 @@
-package br.com.rchlo.service;public class ProductColorsReport {
+package br.com.rchlo.service;
+
+import br.com.rchlo.domain.Product;
+import br.com.rchlo.dto.ProductsReportResult;
+
+import java.util.List;
+
+public class ProductColorsReport {
+
+    public ProductsReportResult report(List<Product> products) {
+        var reportResult = new ProductsReportResult();
+        products.forEach(product -> reportResult.addColor(product.getColor()));
+        return reportResult;
+    }
+
 }
